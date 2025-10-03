bde1c5ebfe5ffdb7c6001f6aa62f15a24ba55d1d
Suman <bagadesuman23@gmail.com>
2025-10-03 22:58:52 +0530

    logger and exception handling are added. Also, the demo is updated

---
+++ demo.py
@@ -0,0 +1,10 @@
+from us_visa.logger import logging
+# logging.info("Welcome to our logging")
+
+from us_visa.exception import USvisaException
+import sys
+
+try:
+    x = 1/0
+except Exception as e:
+   logging.error( USvisaException(e,sys))
